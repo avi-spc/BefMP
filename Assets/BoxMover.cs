a81bd52029fe9a3fe1c36fcf1ed562a0cb9f38c1
avi-spc <avdhesh7394vn@gmail.com>
2018-01-30 04:28:21 +0530

    Playing Project

---
+++ Assets/BoxMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BoxMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            GetComponent<Rigidbody>().transform.Translate(new Vector3(0, 0, 1) * 6 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            GetComponent<Rigidbody>().transform.Translate(new Vector3(0, 0, -1) * 6 * Time.deltaTime);
+        }
+	}
+
+    
+}
